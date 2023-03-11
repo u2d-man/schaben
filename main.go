615189c793aa352aaa723a25eb6ed1bf94686b97
UserKazun <kazuusjlo8@gmail.com>
2023-03-11 21:30:42 +0900

    init CLI

--- main.go
+++ main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+)
+
+const (
+	ExitCodeOK             = 0
+	ExitCodeParseFlagError = 1
+	ExitCodeFail           = 1
+)
+
+type CLI struct {
+	outStream, errStream io.Writer
+}
+
+func NewCLI(outStream, errStream io.Writer) *CLI {
+	return &CLI{outStream: outStream, errStream: errStream}
+}
 
 func main() {
-	fmt.Println("Hello world")
+	cmd := NewCLI(os.Stdout, os.Stderr)
+	os.Exit(cmd.Execute(os.Args))
+}
+
+func (c *CLI) Execute(args []string) int {
+	flags := flag.NewFlagSet("schaben", flag.ContinueOnError)
+	flags.SetOutput(os.Stderr)
+
+	var tURL string
+	flags.StringVar(&tURL, "u", "", "scraping target url")
+
+	err := flags.Parse(args[1:])
+	if err != nil {
+		return ExitCodeParseFlagError
+	}
+
+	fmt.Println(tURL)
+
+	return 0
 }
