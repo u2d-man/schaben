a42b810c08f7033abad0c6d259ec056da6e58496
kazunari.ueeda <34956483+UserKazun@users.noreply.github.com>
2023-05-28 23:01:43 +0900

    Delete docker/mysql/migrate directory

--- docker/mysql/migrate/2305282212_create_table_crawler_site.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE crawler_site
-(
-    id         int auto_increment PRIMARY KEY,
-    domain     varchar(100) not null,
-    url        varchar(100) not null,
-    created_at datetime default CURRENT_TIMESTAMP,
-    updated_at datetime default CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-)
