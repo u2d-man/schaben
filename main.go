b7f922f84342bd48451de210d9ec780f9bf23b1f
UserKazun <developer.kazu0941@gmail.com>
2023-05-28 21:42:19 +0900

    delete flags

--- main.go
+++ main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"flag"
 	"fmt"
+	"github.com/PuerkitoBio/goquery"
 	"io"
 	"net/http"
 	"os"
-
-	"github.com/PuerkitoBio/goquery"
 )
 
 const (
@@ -26,26 +24,11 @@ func NewCLI(outStream, errStream io.Writer) *CLI {
 
 func main() {
 	cmd := NewCLI(os.Stdout, os.Stderr)
-	os.Exit(cmd.Execute(os.Args))
+	os.Exit(cmd.execute())
 }
 
-func (c *CLI) Execute(args []string) int {
-	flags := flag.NewFlagSet("schaben", flag.ContinueOnError)
-	flags.SetOutput(os.Stderr)
-
-	var url string
-	flags.StringVar(&url, "u", "", "scraping target url")
-
-	err := flags.Parse(args[1:])
-	if err != nil {
-		return ExitCodeParseFlagError
-	}
-
-	return c.run(url)
-}
-
-func (c *CLI) run(url string) int {
-	resp, err := http.Get(url)
+func (c *CLI) execute() int {
+	resp, err := http.Get("")
 	if err != nil {
 		_, _ = fmt.Fprintln(c.errStream, err.Error())
 		return ExitCodeFail
@@ -71,3 +54,31 @@ func (c *CLI) run(url string) int {
 
 	return ExitCodeOK
 }
+
+//func (c *CLI) run() int {
+//	resp, err := http.Get(url)
+//	if err != nil {
+//		_, _ = fmt.Fprintln(c.errStream, err.Error())
+//		return ExitCodeFail
+//	}
+//
+//	defer func(Body io.ReadCloser) {
+//		err := Body.Close()
+//		if err != nil {
+//			_, _ = fmt.Fprintln(c.errStream, err.Error())
+//		}
+//	}(resp.Body)
+//
+//	doc, err := goquery.NewDocumentFromReader(resp.Body)
+//	if err != nil {
+//		_, _ = fmt.Fprintln(c.errStream, err.Error())
+//		return ExitCodeFail
+//	}
+//
+//	sec := doc.Find("h1")
+//	sec.Each(func(i int, s *goquery.Selection) {
+//		fmt.Println(s.Text())
+//	})
+//
+//	return ExitCodeOK
+//}
