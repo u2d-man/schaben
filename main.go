9fb09876342b5548519541dbb831c000c543a11a
UserKazun <developer.kazu0941@gmail.com>
2023-05-28 15:01:30 +0900

    not use deprecated goquery method

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"net/http"
 	"os"
 
 	"github.com/PuerkitoBio/goquery"
@@ -32,24 +33,41 @@ func (c *CLI) Execute(args []string) int {
 	flags := flag.NewFlagSet("schaben", flag.ContinueOnError)
 	flags.SetOutput(os.Stderr)
 
-	var tURL string
-	flags.StringVar(&tURL, "u", "", "scraping target url")
+	var url string
+	flags.StringVar(&url, "u", "", "scraping target url")
 
 	err := flags.Parse(args[1:])
 	if err != nil {
 		return ExitCodeParseFlagError
 	}
 
-	return run(tURL)
+	return c.run(url)
 }
 
-func run(tURL string) int {
-	doc, err := goquery.NewDocument(tURL)
+func (c *CLI) run(url string) int {
+	resp, err := http.Get(url)
 	if err != nil {
-		panic(err)
+		_, _ = fmt.Fprintln(c.errStream, err.Error())
+		return ExitCodeFail
 	}
 
-	fmt.Println(doc)
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			_, _ = fmt.Fprintln(c.errStream, err.Error())
+		}
+	}(resp.Body)
 
-	return 0
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		_, _ = fmt.Fprintln(c.errStream, err.Error())
+		return ExitCodeFail
+	}
+
+	sec := doc.Find("h1")
+	sec.Each(func(i int, s *goquery.Selection) {
+		fmt.Println(s.Text())
+	})
+
+	return ExitCodeOK
 }
